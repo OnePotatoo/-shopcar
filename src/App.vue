ebab2c0187336d40927905a64a75fa2770c44171
yyh <958182321@qq.com>
2022-06-26 17:48:45 +0800

    购物车-请求数据

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,28 @@
 import MyHeader from '@/components/MyHeader.vue'
 import MyGoods from '@/components/MyGoods.vue'
 import MyFooter from '@/components/MyFooter.vue'
+import axios from 'axios'
 export default {
   components: {
     MyHeader,
     MyGoods,
     MyFooter
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    async getlist() {
+      const res = await axios({ url: '/api/cart' })
+      // console.log(res)
+      this.list = res.data.list
+      // console.log(this.list)
+    }
+  },
+  created() {
+    this.getlist()
   }
 }
 </script>
