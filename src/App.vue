475669ad0bcd46015a68c9dc7fb08d7919de0bf8
yyh <958182321@qq.com>
2022-06-26 17:30:12 +0800

    购物车-项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>你好</div>
+  <div>
+    <MyHeader />
+    <div class="main">
+      <MyGoods />
+    </div>
+    <MyFooter />
+  </div>
 </template>
 
 <script>
+import MyHeader from '@/components/MyHeader.vue'
+import MyGoods from '@/components/MyGoods.vue'
+import MyFooter from '@/components/MyFooter.vue'
 export default {
-  data() {}
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+.main {
+  padding: 50px 0;
+}
+</style>
